90c17b4dceabb22e224153667e086816986eb329
Shivanand Nalgire <62739431+shivaiitb21@users.noreply.github.com>
2023-02-27 12:13:07 +0530

    Add files via upload

---
+++ mergeexcel_singledir.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import glob
+
+
+
+path = "E:\\Prayog_IITB\\Test\\" #Change path to the directory where you have your input excel files
+
+# read all the files with extension .xlsx i.e. excel 
+filenames = glob.glob(path + "\*.xlsx")
+print('File names:', filenames)
+
+# empty data frame for the new output excel file with the merged excel files
+outputxlsx = pd.DataFrame()
+
+# for loop to iterate all excel files
+for file in filenames:
+   # using concat for excel files
+   # after reading them with read_excel()
+   df = pd.concat(pd.read_excel( file, sheet_name=None), ignore_index=True, sort=False)
+
+   # appending data of excel files
+   outputxlsx = outputxlsx.append(df, ignore_index=True)
+
+print('Final Excel sheet now generated at the same location:')
+outputxlsx.to_excel("E:/Prayog_IITB/Test/Output.xlsx", index=False) #Give the path where you want to store merged output file
